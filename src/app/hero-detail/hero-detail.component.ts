8f297aadb332ebe0e0b15b9aa92b41ab261ecb1f
Wataru Kume <kumewata@gmail.com>
2020-12-31 21:10:36 +0900

    add CRUD with in-memory-data

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -26,10 +26,18 @@ export class HeroDetailComponent implements OnInit {
     this.getHero();
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
